4ec1651741ec16816652e472466f098a38f7180c
Mursaleen00 <playessport@gmail.com>
2023-05-12 15:56:07 +0500

    Extra Assigment Half Completed

---
+++ Assignment/app.js
@@ -0,0 +1,29 @@
+function Marksheet() {
+    var firstName = document.getElementById("FirstName").value
+    var lastName = document.getElementById("LastName").value
+    var rollNumber = +document.getElementById("RollNumber").value
+    var english = +document.getElementById("English").value
+    var math = +document.getElementById("Math").value
+    var urdu = +document.getElementById("Urdu").value
+    var science = +document.getElementById("Science").value
+    var computer = +document.getElementById("Computer").value
+    var islamyat = +document.getElementById("Islamyat").value
+
+    var totleNumber = english + math + urdu + science + computer + islamyat;
+    var persontage = Math.round(totleNumber / 600 * 100);
+
+
+    document.getElementById("fullname").innerHTML = firstName + " " + lastName;
+    document.getElementById("englishscore").innerHTML = english;
+    document.getElementById("mathscore").innerHTML = math;
+    document.getElementById("computerscore").innerHTML = computer;
+    document.getElementById("sciencescore").innerHTML = science;
+    document.getElementById("urduscore").innerHTML = urdu;
+    document.getElementById("ismamyatscore").innerHTML = islamyat;
+    document.getElementById("totalmarks").innerHTML = totleNumber + " / 600";
+    document.getElementById("persontage").innerHTML = persontage + "% / 100%";
+
+    document.getElementById("result").style.display = "block";
+    document.getElementById("main")[0].style.display = "none";
+    document.getElementById("Card").style.color = "blue";
+};
